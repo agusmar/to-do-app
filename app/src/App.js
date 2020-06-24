85615406d16e8c98f070680fc06daa27f8e23045
Agus Mar <agustinam@gmail.com>
2020-06-24 16:51:41 -0300

    initial commit

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React { useState } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        React to do
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function TodoForm() {
+  const [todo, setTodo] = useState({
+    id: "",
+    task:"",
+    completed: false
+  })
+
+  function handleTaskInputChange(e) {
+    setTodo({ ...todo, task: e.target.value});
+  }
+
+  return (
+    <form> 
+      <input 
+        name="task"
+        type="text"
+        value={todo.task}
+        onChange={handleTaskInputChange}
+      />
+      <button type="submit"/>
+    </form>
+  );
+}
+
+export default TodoForm;
